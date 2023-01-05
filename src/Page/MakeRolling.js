d6905f7cb53ef3241edbe3ae95d9b7be89f2304b
Jung Yunho <a01049048063@gmail.com>
2023-01-05 23:44:23 +0900

    chore: postcss 세팅

---
+++ src/Page/MakeRolling.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import MakeRoll from './MakeRollingMypage';
+import RollTitle from './MakeRollingTitle';
+import RollText from './MakeRollingText';
+import Theme from './MakeRollingTheme';
+
+function MakeRolling() {
+  return (
+    <div>
+      <MakeRoll />
+      <RollTitle />
+      <RollText />
+      <Theme />
+    </div>
+  );
+}
+
+export default MakeRolling;
