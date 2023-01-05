10ca11f1f058dbd8dfe0f644d1793ccac18272fb
김민석 <skyfox001015@tukorea.ac.kr>
2023-01-05 18:03:41 +0900

    feat:login페이지 리셋

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -1,14 +1,9 @@
-/* eslint-disable react/button-has-type */
 import React from 'react';
 
 function Login() {
   return (
     <div>
-      <h1>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Button
-        </button>
-      </h1>
+      <h1>Login</h1>
     </div>
   );
 }
