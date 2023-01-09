df65e62e1a96aab11c4ff5304719cdba4c8488fc
김민석 <skyfox001015@tukorea.ac.kr>
2023-01-10 02:09:04 +0900

    formik,yup구현완료

--- src/Page/Register.js
+++ src/Page/Register.js
@@ -1,10 +1,121 @@
 import React from 'react';
-
+import styled from 'styled-components';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-// import 'bootstrap/dist/css/bootstrap.css';
+import './Background.css';
 
+import ID from '../Image/ID.png';
+import PW from '../Image/PW.png';
+
+// 배경화면을 꽉 채워주기 위한 divx태그입니다
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+// 회원가입 Text를 감싸줍니다
+const TextWrap = styled.div`
+  text-align: center; /*"회원가입"을 가운데 정렬시켜줍니다*/
+  @media screen and (max-width: 63rem) {
+    display: none; /*화면이 작아지면 "회원가입 글씨를 사라지게 합니다"*/
+  }
+`;
+
+// "회원가입"div 태그입니다
+const RegText = styled.div`
+  display: flex;
+  font-size: 2.5rem;
+  font-weight: 800;
+  display: inline-block;
+  margin-bottom: 3rem;
+`;
+
+// 스케치북 안에 있는 모든 요소들을 감싸줍니다
+const KeysWrap = styled.div`
+  position: absolute;
+  top: 55%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 10;
+  @media screen and (max-width: 63rem) {
+    top: 55%;
+  }
+`;
+
+// 아이디, 패스워드, 이메일, 닉네임을 "props"로 바꿔가면서 재사용할 수 있음
+const KeyWrap = styled.div`
+  width: 45rem;
+  height: 3.1rem;
+  border-radius: ${props => props.border};
+  background: #fff;
+  border-width: 0.063rem;
+  border-color: #000;
+  box-shadow: 0rem 0.25rem 0.25rem 0 rgba(0, 0, 0, 0.25);
+  display: flex;
+  margin-bottom: 1rem;
+  /*화면이 작아지면 크기를 작게하고 겉에 radius속성을 없애줍니다*/
+  @media screen and (max-width: 63rem) {
+    width: 37.5rem;
+    height: 3.125rem;
+    border-radius: 0;
+  }
+`;
+
+// Icon 태그입니다
+const IconImg = styled.img`
+  width: 2.5rem;
+  height: 2.5rem;
+  text-align: center;
+  display: flex;
+  margin: auto 1rem auto 1rem;
+`;
+
+// 아이디,패스워드,이메일,닉네임을 입력하는 input태그입니다
+// const KeyInput = styled.input`
+//   //placeholder스타일
+//   width: 2.25rem;
+//   height: 2.5rem;
+//   font-size: 1.5rem;
+//   font-weight: 500;
+//   /////
+//   padding-left: 0.5rem;
+//   margin: auto 1rem auto 0rem;
+//   width: 31.25rem;
+//   &:focus {
+//     outline: none;
+//   }
+// `;
+
+// // 로그인, 회원가입을 감싸기 위한 div태그입니다
+// const BtnWrap = styled.div`
+//   text-align: center;
+//   display: block;
+// `;
+
+// 회원가입 버튼
+const SignupBtn = styled.button`
+  width: 16.25rem;
+  height: 2.813rem;
+  border-radius: 0.813rem;
+  background: #3a3a3a;
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: #fff;
+  display: block;
+  margin: 3rem auto 1.2rem;
+`;
+
+// 로그인 버튼
+const LoginBtn = styled.button`
+  width: 4.438rem;
+  height: 1.75rem;
+  font-size: 1.125rem;
+  font-weight: 700;
+  margin-top: 2rem;
+  display: block;
+  margin: 0rem auto 0rem;
+`;
 const LoginSchema = Yup.object().shape({
   email: Yup.string()
     .email('Invalid email address format')
@@ -16,14 +127,12 @@ const LoginSchema = Yup.object().shape({
 
 function Register() {
   return (
-    <div className="container">
-      <div className="row mb-5">
-        <div className="col-lg-12 text-center">
-          <h1 className="mt-5">Login Form</h1>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-lg-12">
+    <div className="register">
+      <Background>
+        <KeysWrap>
+          <TextWrap>
+            <RegText>회원가입</RegText>
+          </TextWrap>
           <Formik
             initialValues={{ email: '', password: '' }}
             validationSchema={LoginSchema}
@@ -35,25 +144,17 @@ function Register() {
           >
             {({ touched, errors, isSubmitting }) => (
               <Form>
-                <div className="form-group">
-                  <div>Email</div>
-                  <Field
-                    type="email"
-                    name="email"
-                    placeholder="Enter email"
-                    className={`form-control ${
-                      touched.email && errors.email ? 'is-invalid' : ''
-                    }`}
-                  />
-                  <ErrorMessage
-                    component="div"
-                    name="email"
-                    className="invalid-feedback"
-                  />
-                </div>
-
-                <div className="form-group">
-                  <div>Password</div>
+                <KeyWrap border="0.938rem 0.938rem 0 0">
+                  <IconImg src={ID} alt="" />
+                  <Field type="email" name="email" placeholder="Enter email" />
+                </KeyWrap>
+                <ErrorMessage
+                  component="div"
+                  name="email"
+                  className="invalid-feedback"
+                />
+                <KeyWrap>
+                  <IconImg src={PW} alt="" />
                   <Field
                     type="password"
                     name="password"
@@ -62,25 +163,58 @@ function Register() {
                       touched.password && errors.password ? 'is-invalid' : ''
                     }`}
                   />
-                  <ErrorMessage
-                    component="div"
+                </KeyWrap>
+                <ErrorMessage
+                  component="div"
+                  name="password"
+                  className="invalid-feedback"
+                />
+                <KeyWrap>
+                  <IconImg src={ID} alt="" />
+                  <Field
+                    type="password"
                     name="password"
-                    className="invalid-feedback"
+                    placeholder="Enter password"
+                    className={`form-control ${
+                      touched.password && errors.password ? 'is-invalid' : ''
+                    }`}
                   />
-                </div>
+                </KeyWrap>
+                <ErrorMessage
+                  component="div"
+                  name="password"
+                  className="invalid-feedback"
+                />
+                <KeyWrap border="0 0 0.938rem 0.938rem">
+                  <IconImg src={ID} alt="" />
+                  <Field
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    className={`form-control ${
+                      touched.password && errors.password ? 'is-invalid' : ''
+                    }`}
+                  />
+                </KeyWrap>
+                <ErrorMessage
+                  component="div"
+                  name="password"
+                  className="invalid-feedback"
+                />
 
-                <button
+                <SignupBtn
                   type="submit"
                   className="btn btn-primary btn-block"
                   disabled={isSubmitting}
                 >
-                  {isSubmitting ? 'Please wait...' : 'Submit'}
-                </button>
+                  {isSubmitting ? 'Please wait...' : '회원가입'}
+                </SignupBtn>
+                <LoginBtn>로그인</LoginBtn>
               </Form>
             )}
           </Formik>
-        </div>
-      </div>
+        </KeysWrap>
+      </Background>
     </div>
   );
 }
