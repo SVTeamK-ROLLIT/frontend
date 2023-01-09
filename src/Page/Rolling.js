2b629050da0e4320684f4ab250377f1ee8445c84
Jung Yunho <a01049048063@gmail.com>
2023-01-09 10:58:48 +0900

    feat: 마이페이지 버튼 추가

--- src/Page/Rolling.js
+++ src/Page/Rolling.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+
+// 배경화면을 꽉 채워주기 위한 divx태그입니다
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+// 마이페이지 버튼입니다.
+const Mypagebtn = styled.button`
+  width: 199px;
+  height: 48px;
+  font-size: 40px;
+  font-weight: 700;
+  text-align: left;
+  color: black;
+`;
+// 마이페이지 영역을 나눠주기
+const BtnWrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
 
 function Rolling() {
   return (
-    <div>
-      <h1>Rolling</h1>
-    </div>
+    <Background>
+      <BtnWrap>
+        <Mypagebtn>마이페이지</Mypagebtn>
+      </BtnWrap>
+    </Background>
   );
 }
 
