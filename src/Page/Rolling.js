a1d2f4bc4b177c13ef5c139da2e9a1160c14f1d2
Jung Yunho <a01049048063@gmail.com>
2023-01-09 11:37:14 +0900

    feat: 제목, 인원수 표시

--- src/Page/Rolling.js
+++ src/Page/Rolling.js
@@ -15,18 +15,55 @@ const Mypagebtn = styled.button`
   text-align: left;
   color: black;
 `;
-// 마이페이지 영역을 나눠주기
-const BtnWrap = styled.div`
+// 마이페이지버튼 영역을 나눠주기
+const TopWrap = styled.div`
   display: flex;
   justify-content: flex-end;
 `;
 
+// 제목 영역을 나눠주기
+const TitleWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+// 제목을 생성합니다
+const RollpaperTitle = styled.div`
+  width: 325px;
+  height: 50px;
+  font-size: 50px;
+  font-weight: 700;
+  text-align: center;
+  color: black;
+`;
+// 인원수 표시
+const PeopleNum = styled.div`
+  width: 50px;
+  height: 20px;
+  font-size: 20px;
+  font-weight: 700;
+  /* text-align: ; */
+  color: black;
+  margin-bottom: 100px;
+  margin-left: 500px;
+`;
+// 인원수 영역
+const PeopleWrap = styled.div`
+  display: flex;
+  justify-content: right;
+`;
 function Rolling() {
   return (
     <Background>
-      <BtnWrap>
+      <TopWrap>
         <Mypagebtn>마이페이지</Mypagebtn>
-      </BtnWrap>
+      </TopWrap>
+      <TitleWrap>
+        <RollpaperTitle>TO. 모두들</RollpaperTitle>
+      </TitleWrap>
+      <PeopleWrap>
+        <PeopleNum>13명</PeopleNum>
+      </PeopleWrap>
     </Background>
   );
 }
