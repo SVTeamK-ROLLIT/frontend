0d18bad71e7d009dfaf02c86eb9c4a5dd02e1d14
김민석 <skyfox001015@tukorea.ac.kr>
2023-01-11 16:09:32 +0900

    feat: rolling페이지 css수정 완료

--- src/Page/GlobalStyle.js
+++ src/Page/GlobalStyle.js
@@ -2,12 +2,12 @@ import { createGlobalStyle } from 'styled-components';
 import sketchbook from '../Image/sketchbookLogin.png';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  body .rolling {
     margin: 0;
     padding: 0;
     background: url(${sketchbook});
     background-repeat: no-repeat;
-    background-size: 100rem 70rem;
+    background-size: 100rem 60rem;
     background-position: center;
   }
 `;
--- src/Page/Rolling.js
+++ src/Page/Rolling.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
-import sketchbook from '../Image/sketchbookLogin.png';
+// import sketchbook from '../Image/sketchbookLogin.png';
+import blackboard from '../Image/blackboard.png';
 
-const BackgroundImg = styled.div`
-  background-color: #fcedb0;
-  background-size: cover;
-  background: no-repeat;
-  background-image: url(${sketchbook});
-  margin: auto;
-  width: 100vw;
-  height: 100vh;
-`;
+// import GlobalStyle from './GlobalStyle';
 
-// // 마이페이지 버튼입니다.
+// 배경화면을 꽉 채워주기 위한 divx태그입니다
+// const Background = styled.div`
+//   width: 100vw;
+//   height: 100vh;
+// `;
+
+// const BackgroundImg = styled.div`
+//   /* width: 100%;
+//   height: 100%; */
+//   // border: 5px solid #535353;
+//   background-color: #fcedb0;
+//   background-size: 80rem 70rem;
+//   background-image: url(${sketchbook});
+//   margin: auto;
+// `;
+
+// 마이페이지 버튼입니다.
 // const Mypagebtn = styled.button`
 //   width: 199px;
 //   height: 48px;
@@ -80,7 +89,7 @@ const BackgroundImg = styled.div`
 //   /* position: fixed; */
 // `;
 
-// 버튼 박스 박스
+// // 버튼 박스 박스
 // const Wrap = styled.div`
 //   height: 100vh;
 //   /* background-color: yellow; */
@@ -89,10 +98,64 @@ const BackgroundImg = styled.div`
 //   justify-content: flex-end;
 // `;
 
+const SketchBookImg = styled.div`
+  // border: 5px solid #535353;
+  height: 100%;
+  /* background-color: #fcedb0; */
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position-x: center;
+  background-image: url(${blackboard});
+  margin: 0 auto;
+`;
+
+const AllWrap = styled.div`
+  /* background-color: red; */
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  height: 100%;
+  z-index: 90;
+`;
+const MyPageBtn = styled.button`
+  margin: 2rem 14rem 0 auto;
+  /* background-color: red; */
+  height: 4rem;
+  display: block;
+  width: 12rem;
+  font-size: 40px;
+  font-weight: 700;
+  color: #fff;
+  //텍스트 검정 태두리 주기
+  text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000;
+  line-height: 47px;
+`;
+const Text = styled.div`
+  /* background-color: red; */
+  height: 5rem;
+  width: 100%;
+
+  text-align: center;
+  font-size: 50px;
+  font-weight: 700;
+`;
+const MemoWrap = styled.div`
+  /* background-color: yellow; */
+  height: 50rem;
+  width: 80rem;
+  margin: 0 auto;
+`;
+
 function Rolling() {
   return (
-    <div>
-      <BackgroundImg />
+    <div className="rolling">
+      <SketchBookImg>
+        <AllWrap>
+          <MyPageBtn>마이페이지</MyPageBtn>
+          <Text>to.Team_k</Text>
+          <MemoWrap />
+        </AllWrap>
+      </SketchBookImg>
     </div>
   );
 }
