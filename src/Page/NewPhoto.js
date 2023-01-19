0138647befeb6ab59b578c203eef1abc95c96435
김민석 <skyfox001015@naver.com>
2023-01-19 17:24:37 +0900

    feat : StyledRect 적용전

--- src/Page/NewPhoto.js
+++ src/Page/NewPhoto.js
@@ -1,59 +1,62 @@
 // import styled from 'styled-components';
-import React, { useState, useRef } from 'react';
-import Draggable from 'react-draggable';
+import React, { useState } from 'react';
+import ResizableRect from 'react-resizable-rotatable-draggable';
 // import img from '../Image/ID.png';
-import { Resizable } from 're-resizable';
-import { red } from '@mui/material/colors';
 
-export default function NewPhoto({ photo, setCoor }) {
-  const nodeRef = useRef(null);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  //   const [img,setImg]=useState('');
+export default function NewPhoto() {
+  const [width2, setWidth] = useState(200);
+  const [height2, setHeight] = useState(100);
+  const [top2, setTop] = useState(100);
+  const [left2, setLeft] = useState(100);
+  const [rotateAngle, setRotateAngle] = useState(0);
 
-  const [Opacity, setOpacity] = useState(false);
-
-  console.log(Opacity);
-  const trackPos = data => {
-    setPosition({ x: data.x, y: data.y });
+  // eslint-disable-next-line no-unused-vars
+  const handleResize = (style, isShiftKey, type) => {
+    let { top, left, width, height } = style;
+    top = Math.round(top);
+    left = Math.round(left);
+    width = Math.round(width);
+    height = Math.round(height);
+    setWidth(width);
+    setHeight(height);
+    setTop(top);
+    setLeft(left);
+  };
+  const handleRotate = rotateAngle2 => {
+    setRotateAngle(rotateAngle2);
   };
 
-  const handleStart = () => {
-    setOpacity(true);
-  };
-  const handleEnd = () => {
-    setOpacity(false);
-    setCoor(position);
-  };
-
-  const [rawLog, setRawLog] = useState();
-  const reader = new FileReader();
-  reader.readAsDataURL(photo); // 파일을 읽는 메서드
-  reader.onload = () => {
-    setRawLog(reader.result);
-    console.log(rawLog);
+  const handleDrag = (deltaX, deltaY) => {
+    setLeft(left2 + deltaX);
+    setTop(top2 + deltaY);
   };
 
   return (
-    <Draggable
-      bounds="parent"
-      nodeRef={nodeRef}
-      onDrag={(e, data) => trackPos(data)}
-      onStart={handleStart}
-      onStop={handleEnd}
-      defaultPosition={{ x: position.x, y: position.y }}
-    >
-      <Resizable
-        defaultSize={{
-          width: 320,
-          height: 200,
-        }}
-        style={{
-          backgroundColor: red,
-        }}
-      >
-        Sample with default size
-      </Resizable>
-    </Draggable>
+    <div className="App">
+      <ResizableRect
+        left={left2}
+        top={top2}
+        width={width2}
+        height={height2}
+        rotateAngle={rotateAngle}
+        minWidth={100} // 최소크기
+        // aspectRatio={false}
+        // minWidth={10}
+        // minHeight={10}
+        zoomable="n, w, s, e, nw, ne, se, sw"
+        // rotatable={true}
+        // onRotateStart={this.handleRotateStart}
+        onRotate={handleRotate}
+        // onRotateEnd={this.handleRotateEnd}
+        // onResizeStart={this.handleResizeStart}
+        onResize={handleResize}
+        // onResizeEnd={this.handleUp}
+        // onDragStart={this.handleDragStart}
+        onDrag={handleDrag}
+        // onDragEnd={this.handleDragEnd}
+        style={{ backgroundColor: 'red' }}
+      />
+    </div>
   );
 }
 
