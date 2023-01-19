789191625e78cb9fc4819c56300e578705ab5759
Jung Yunho <a01049048063@gmail.com>
2023-01-20 05:43:14 +0900

    feat: 사진 업로드해서  url 반환

--- src/Page/Cartoonize.js
+++ src/Page/Cartoonize.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 // Import React FilePond
@@ -20,48 +20,24 @@ const CartoonBtn = styled.button`
 `;
 
 function Cartoonize({ files }) {
-  const [resultImage, setResultImage] = useState({});
+  const [url, seturl] = useState('');
 
   const onSubmit = async () => {
-    const formData = new FormData();
-    formData.append('image', files[0].file);
-    const response = await axios.post(
-      'http://127.0.0.1:8080/api/v1/photos',
-      formData,
-    );
-    console.log(response.data);
-    const response2 = await axios.post(
-      'http://127.0.0.1:8080/api/v1/papers/cartoons',
-      response.data,
-    );
-    console.log(response2.data);
-    let count = 0;
-
-    const interval = await setInterval(async () => {
-      const result = await axios.post(
-        'http://127.0.0.1:8080/api/v1/papers/cartoons/results',
-        response2.data,
+    try {
+      const formData = new FormData();
+      formData.append('image', files[0].file);
+      const response = await axios.post(
+        'http://127.0.0.1:8080/api/v1/photos',
+        formData,
       );
-      console.log('함수내부2', resultImage);
-
-      if (result.data.message === 'still working') {
-        count += 1;
-      } else if (result.data.url) {
-        setResultImage(result.data.url);
-        clearInterval(interval);
-        console.log('* get result success *');
-        console.log('함수내부1', resultImage);
-      } else if (count <= 20) {
-        // interval();
-        count += 1;
-      } else {
-        clearInterval(interval);
-        count += 1;
-        console.log('* time out *');
-      }
-    }, 1000);
-    console.log('함수외부', resultImage);
+      //   const s3url = response.data;
+      //   console.log(s3url);
+      seturl(response.data.image_url);
+    } catch (err) {
+      console.log('Error >>', err);
+    }
   };
+  console.log(url); // url까지 반환 성공
 
   return (
     <CartoonBtn type="button" onClick={onSubmit}>
