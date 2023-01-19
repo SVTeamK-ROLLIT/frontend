cb510792a496cad8ac9385d8423a956f09088caa
Jung Yunho <a01049048063@gmail.com>
2023-01-20 05:43:14 +0900

    feat: 사진 업로드해서  url 반환

--- src/Page/FilePondTemplate.js
+++ src/Page/FilePondTemplate.js
@@ -22,6 +22,7 @@ import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 import axios from 'axios';
 
 import { BsX } from 'react-icons/bs';
+import Cartoonize from './Cartoonize';
 
 const SumbitBtn = styled.button`
   width: 100px;
@@ -149,6 +150,7 @@ function FilePondTemplate({
         >
           업로드
         </SumbitBtn>
+        <Cartoonize files={files} />
       </Modal>
     </div>
   );
