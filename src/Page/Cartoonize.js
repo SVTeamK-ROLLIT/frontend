2d66afbf1fd39612ae94e0b98afc6aadc1a222f7
Jung Yunho <a01049048063@gmail.com>
2023-01-20 17:35:40 +0900

    feat: 폴링구현 ( 비동기성 오류남음)

--- src/Page/Cartoonize.js
+++ src/Page/Cartoonize.js
@@ -102,8 +102,6 @@ function Cartoonize({
   };
   console.log('@@@@@', imageUrl);
 
-  //   console.log(response.data.url);
-
   return (
     <CartoonBtn type="button" onClick={onSubmit}>
       <ToastContainer />
