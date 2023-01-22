1c416f3fe11f8bb995d3582dd1a2656444c207dd
김민석 <skyfox001015@naver.com>
2023-01-23 00:48:37 +0900

    feat : 만들기 버튼 위치 잡기 완료

--- src/Page/MemoText.js
+++ src/Page/MemoText.js
@@ -143,9 +143,9 @@ function MemoText({
                 name="text"
                 className="invalid-feedback"
               />
-              <MakeBtn type="submit">만들기</MakeBtn>
             </SketchbookImg>
             <PencilImg src={pencil} />
+            <MakeBtn type="submit">만들기</MakeBtn>
           </InputWrap>
         </Form>
       )}
