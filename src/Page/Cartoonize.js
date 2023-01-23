611b4f90b70b0f8eb2b981fddbc0182e03630592
Jung Yunho <a01049048063@gmail.com>
2023-01-24 00:50:09 +0900

    feat: 비동기 함수 구현중

--- src/Page/Cartoonize.js
+++ src/Page/Cartoonize.js
@@ -18,9 +18,42 @@ const CartoonBtn = styled.button`
   height: 10rem;
   background-color: red;
 `;
+function MyComponent({ taskId }) {
+  const [result, setResult] = useState(null);
+  const [count, setCount] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
 
-function Cartoonize({ files }) {
+  useEffect(() => {
+    if (intervalId === null) {
+      const id = setInterval(async () => {
+        const response = await axios.post(
+          'http://127.0.0.1:8080/api/v1/papers/cartoons/results',
+          { taskId },
+        );
+        // eslint-disable-next-line
+        const data = response.data;
+        setResult(data.status);
+        if (data.status !== 'still working' || count >= 10) {
+          clearInterval(intervalId);
+          setIntervalId(null);
+        }
+        setCount(count + 1);
+      }, 2000);
+      setIntervalId(id);
+    }
+  }, [intervalId, count, taskId]);
+  console.log('result', result);
+}
+function Cartoonize({
+  files,
+  isOpen,
+  closeModal,
+  setisActivate,
+  setIsPhoto,
+  setPhoto,
+}) {
   const [resultImage, setResultImage] = useState({});
+  const [imageUrl, setImageUrl] = useState(null);
 
   const onSubmit = async () => {
     const formData = new FormData();
@@ -35,41 +68,47 @@ function Cartoonize({ files }) {
       response.data,
     );
     console.log(response2.data);
-    let count = 0;
+    // let count = 0;
 
-    const interval = await setInterval(async () => {
-      const result = await axios.post(
-        'http://127.0.0.1:8080/api/v1/papers/cartoons/results',
-        response2.data,
-      );
-      console.log('함수내부2', resultImage);
+    // const interval = setInterval(async () => {
+    //   const result = await axios.post(
+    //     'http://127.0.0.1:8080/api/v1/papers/cartoons/results',
+    //     response2.data,
+    //   );
+    //   console.log('함수내부2', resultImage);
 
-      if (result.data.message === 'still working') {
-        count += 1;
-      } else if (result.data.url) {
-        setResultImage(result.data.url);
-        clearInterval(interval);
-        console.log('* get result success *');
-        console.log('함수내부1', resultImage);
-      } else if (count <= 20) {
-        // interval();
-        count += 1;
-      } else {
-        clearInterval(interval);
-        count += 1;
-        console.log('* time out *');
-      }
-    }, 1000);
-    console.log('함수외부', resultImage);
-    await useEffect(() => {
-      const s3Url = resultImage;
-      fetch(s3Url)
-        .then(response => response.blob())
-        .then(blob => {
-          const localUrl = URL.createObjectURL(blob);
-          setImageUrl(localUrl);
-        });
-    }, []);
+    //   if (result.data.message === 'still working') {
+    //     count += 1;
+    //   } else if (result.data.url) {
+    //     await setResultImage(result.data.url);
+    //     clearInterval(interval);
+    //     console.log('* get result success *');
+    //     console.log('함수내부1', resultImage);
+    //   } else if (count <= 20) {
+    //     // interval();
+    //     count += 1;
+    //   } else {
+    //     clearInterval(interval);
+    //     count += 1;
+    //     console.log('* time out *');
+    //   }
+    // }, 1000);
+    // console.log('함수외부', resultImage);
+    // // ##########################################################
+    // useEffect(() => {
+    //   const s3Url = resultImage;
+    //   axios({
+    //     url: s3Url,
+    //     method: 'GET',
+    //     responseType: 'blob', // important
+    //   }).then(response3 => {
+    //     const blob = new Blob([response3.data], { type: 'image/jpeg' });
+    //     const localUrl = URL.createObjectURL(blob);
+    //     setImageUrl(localUrl);
+    //   });
+    // });
+    // console.log('찍어줘', imageUrl);
+    MyComponent(response2.data);
   };
 
   return (
