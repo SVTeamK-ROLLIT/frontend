35f5cb63589e0ad355824bd9ca23d67478d42966
junhui <zidane__@naver.com>
2023-01-24 13:19:11 +0900

    feat: Outlog 겹치는 부분 수정완료

--- src/Page/Outlog.js
+++ src/Page/Outlog.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const LogoutWrap = styled.div`
+  position: fixed;
+  top: 7%;
+  left: 90%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  z-index: 999;
+`;
+
 const MypageBtn = styled.button`
   //상단 로그인 버튼
-  width: 20vh;
-  height: 5.5vh;
-  font-size: 3.7vh;
+  width: 12rem;
+  height: 5.5rem;
+
+  font-size: 2.5rem;
   font-weight: 1000;
-  position: absolute;
-  top: 7%;
-  left: 78%;
-  transform: translate(-50%, -50%);
 
   color: white;
   font-family: 'Cafe24Ssurround';
@@ -18,18 +24,15 @@ const MypageBtn = styled.button`
   -webkit-text-stroke-width: 1.1px;
   -webkit-text-stroke-color: black;
 `;
-
 const SignupBtn = styled.button`
   //상단 회원가입 버튼
-  width: 15vh;
-  height: 5.5vh;
-  font-size: 3.7vh;
+  width: 12rem;
+  height: 5.5rem;
+  font-size: 2.5rem;
   font-weight: bold;
   // 스캐치북 위에 올리기
-  position: absolute;
-  top: 7%;
-  left: 88%;
-  transform: translate(-50%, -50%);
+
+  clear: both;
 
   color: white;
   font-family: 'Cafe24Ssurround';
@@ -40,10 +43,10 @@ const SignupBtn = styled.button`
 
 function Onlog({ onClick }) {
   return (
-    <div>
+    <LogoutWrap>
       <MypageBtn>마이페이지</MypageBtn>
       <SignupBtn onClick={onClick}>로그아웃</SignupBtn>
-    </div>
+    </LogoutWrap>
   );
 }
 
