ed205ace1a2e88feb2134233e1c8674a5a873a6f
junhui <zidane__@naver.com>
2023-01-28 04:03:27 +0900

    feat: 마이페이지 디자인 수정 2

--- src/Page/MyPageLine.js
+++ src/Page/MyPageLine.js
@@ -2,22 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 function MyPageLine() {
-  return <Line>ROLLING PAPER LIST</Line>;
+  return <Line>롤링페이퍼 목록</Line>;
 }
 
 export default MyPageLine;
 
 const Line = styled.div`
   width: 50rem;
-  height: 5rem;
+  height: 6rem;
   border-radius: 2rem;
-  margin: 4rem auto 0 auto;
+  margin: 15rem auto 0 auto;
   font-size: 3.5rem;
   text-align: center;
   color: white;
   position: relative;
   font-family: 'Cafe24Ssurround';
-  text-shadow: 1.5px 1.5px 1.5px gray;
+  text-shadow: 1.5px 2px 2px gray;
   -webkit-text-stroke-width: 2px;
   -webkit-text-stroke-color: gray;
 `;
