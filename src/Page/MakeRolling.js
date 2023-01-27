b9c801116dd0ef50887c2c4cd0a098bfb9c49e56
junhui <zidane__@naver.com>
2023-01-28 00:30:29 +0900

    feat: MakeRolling 페이지 수정

--- src/Page/MakeRolling.js
+++ src/Page/MakeRolling.js
@@ -4,18 +4,22 @@ import MakeRoll from './MakeRollingMypage';
 import RollText from './MakeRollingText';
 import Theme from './MakeRollingTheme';
 
+import background from '../Image/welcome1.png';
+
 const AllWrap = styled.div`
   height: 100vh;
   width: 100%;
   z-index: -1;
-  background-color: #fcedb0;
   position: fixed;
+  background-repeat: no-repeat;
+  background-image: url(${background});
+  background-size: cover;
 `;
 
 function MakeRolling() {
   return (
     <div className="makerolling">
-      <AllWrap />
+      <AllWrap src={background} />
       <MakeRoll />
       <RollText />
       <Theme />
--- src/Page/MakeRollingText.js
+++ src/Page/MakeRollingText.js
@@ -2,20 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 function MakeRollingText() {
-  return <Text>롤링페이퍼 테마 선택</Text>;
+  return <Text>제목과 테마를 선택해주세요</Text>;
 }
 
 const Text = styled.div`
   /* width: 387px; */
-  height: 3rem;
-  font-size: 40px;
+  height: 5rem;
+  font-size: 4rem;
   font-weight: 700;
-  color: black;
+  color: whitesmoke;
   text-align: center;
   margin: auto;
   margin-top: 3rem;
   margin-bottom: 1.8rem;
   font-family: 'Cafe24Ssurround';
+  -webkit-text-stroke-width: 1.5px;
+  -webkit-text-stroke-color: black;
+  text-shadow: 2px 2px 1.5px gray;
 `;
 
 export default MakeRollingText;
