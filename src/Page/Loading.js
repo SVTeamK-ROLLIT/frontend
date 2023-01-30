8abc5a498e759eb8240b185a86d38fa36c06b60e
김민석 <skyfox001015@naver.com>
2023-01-30 21:25:55 +0900

    feat : 로딩 스피너 변경

--- src/Page/Loading.js
+++ src/Page/Loading.js
@@ -3,49 +3,27 @@ import './Loading.css';
 
 function Loading() {
   return (
-    <div className="loader">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 100 100"
-        id="circle-middle"
-      >
-        <circle fill="#EDEDED" cx={50} cy={50} r={6} />
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <circle fill="#26A6D1" cx={50} cy={50} r="4.5" />
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <circle fill="#26A6D1" cx={50} cy={50} r="4.5" />
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <circle fill="#26A6D1" cx={50} cy={50} r="4.5" />
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <circle fill="#26A6D1" cx={50} cy={50} r="4.5" />
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <circle fill="#26A6D1" cx={50} cy={50} r="4.5" />
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <circle fill="#26A6D1" cx={50} cy={50} r="4.5" />
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <circle fill="#26A6D1" cx={50} cy={50} r="4.5" />
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <circle fill="#26A6D1" cx={50} cy={50} r="4.5" />
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <circle fill="#26A6D1" cx={50} cy={50} r="4.5" />
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <circle fill="#26A6D1" cx={50} cy={50} r="4.5" />
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <circle fill="#26A6D1" cx={50} cy={50} r="4.5" />
-      </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <circle fill="#26A6D1" cx={50} cy={50} r="4.5" />
+    <div>
+      <div className="container">
+        <div className="dot dot-1" />
+        <div className="dot dot-2" />
+        <div className="dot dot-3" />
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation={10}
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7"
+            />
+          </filter>
+        </defs>
       </svg>
     </div>
   );
